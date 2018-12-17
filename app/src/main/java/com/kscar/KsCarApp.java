681694b8e4cceac607870a77ac48cd5a501fe0dc
rahulkumarit <rk74535@gmail.com>
2018-12-18 02:27:57 +0530

    Initial commit

---
+++ app/src/main/java/com/kscar/KsCarApp.java
@@ -0,0 +1,17 @@
+package com.kscar;
+
+import android.app.Application;
+
+/**
+ * Created by SONI on 12/18/2018.
+ */
+
+public class KsCarApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
---
+++ app/src/main/java/com/kscar/activities/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.kscar.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+/**
+ * Created by SONI on 10/19/2018.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+
+
+}
