9be811b38c6f1b3194de476c3c94de4cf3ac0435
Rahul Kumar <rahulkumar@devrepublic.nl>
2019-01-08 14:03:15 +0530

    working on notification

---
+++ app/src/main/java/com/kscar/notification/MyFirebaseInstanceIDService.java
@@ -0,0 +1,46 @@
+package com.kscar.notification;
+/**
+ * Created by SONI on 12/18/2018.
+ */
+
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.kscar.prefrences.SessionManager;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        // Saving reg id to shared preferences
+        storeRegIdInPref(refreshedToken);
+
+        // sending reg id to your server
+        sendRegistrationToServer(refreshedToken);
+
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
+        registrationComplete.putExtra("token", refreshedToken);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    private void sendRegistrationToServer(final String token) {
+        // sending gcm token to server
+        Log.e(TAG, "sendRegistrationToServer: " + token);
+    }
+
+    private void storeRegIdInPref(String token) {
+        SessionManager sessionManager = new SessionManager(getApplicationContext());
+        sessionManager.setNotificationToken(token);
+    }
+
+}
